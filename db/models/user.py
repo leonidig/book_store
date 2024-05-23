72a9f8f82ffb1fcac5fecb31eddf0b906699256f
Bohdan Savin <bogdan07112009@gmail.com>
2024-05-23 22:56:46 +0300

    Co-authored-by: leonidig <leonidig@users.noreply.github.com> Co-authored-by: UkraineGoose <Nazarbizarre@users.noreply.github.com>

---
+++ db/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from flask_login import UserMixin
+
+
+class Base(DeclarativeBase):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    nickname: Mapped[str]
+    email: Mapped[str]
+    password: Mapped[str]
+
+
+    def is_active(self) -> bool:
+        return True
+    def is_authenticated(self) -> bool:
+        return True
+    def is_anonymous(self)->bool:
+        return False
+    def get_id(self)->str:
+        return f"{self.id}"

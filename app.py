06bb0be902f920cd6a9c6197c8d8b51ae1c8c673
Bohdan Savin <bogdan07112009@gmail.com>
2024-05-24 21:12:16 +0300

    Working main page with all books

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
-
+from requests import get, Response
 
 app = Flask("frontend")
+BACKEND_URL = "http://127.0.0.1:8000"
+
 
 @app.get("/")
 def index():
-    contenx = dict()
+    books = {
+        "books":get(f"{BACKEND_URL}/get_books").json()
+    }
+    print(books)
+    return render_template("index.html", **books)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
